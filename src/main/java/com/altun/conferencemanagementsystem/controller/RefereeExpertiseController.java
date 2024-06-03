9a94091093a1125542e79d4ca7d3d940c104ce48
tarve <tarverdiyevaltun@gmail.com>
2024-06-03 13:57:28 +0400

    RefereeExpertiseController added

--- src/main/java/com/altun/conferencemanagementsystem/controller/RefereeExpertiseController.java
+++ src/main/java/com/altun/conferencemanagementsystem/controller/RefereeExpertiseController.java
@@ -1,12 +1,29 @@
 package com.altun.conferencemanagementsystem.controller;
 
+import com.altun.conferencemanagementsystem.entity.RefereeExpertise;
+import com.altun.conferencemanagementsystem.service.RefereeExpertiseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/expertise")
 @RequiredArgsConstructor
 public class RefereeExpertiseController {
 
+    private final RefereeExpertiseService refereeExpertiseService;
+
+   // @PreAuthorize("hasAuthority('REFEREE')")
+    @PostMapping
+    public ResponseEntity<RefereeExpertise> addExpertise(@RequestParam String username, @RequestParam String expertiseArea) {
+        return ResponseEntity.ok(refereeExpertiseService.addExpertise(username, expertiseArea));
+    }
+
+  //  @PreAuthorize("hasAuthority('REFEREE')")
+    @GetMapping("/{username}")
+    public ResponseEntity<List<RefereeExpertise>> getExpertiseByReferee(@PathVariable String username) {
+        return ResponseEntity.ok(refereeExpertiseService.getExpertiseByReferee(username));
+    }
 }
